2a5bb90f6b1d33fc4080796ea86fb0d5001aa258
Max Celant <massimiliano.celant@aa.com>
2024-11-13 10:28:31 -0600

    Updating lexer

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -1,15 +1,70 @@
 package lexer
 
 type Lexer struct {
-	GlobalPosition  int64
-	CurrentPosition int64
+	// globalPos int64
+	start    int64
+	curr     int64
+	currLine int64
+	tokens   []Token
+}
+
+func String(ch byte) string {
+	return string(ch)
 }
 
 func (l *Lexer) ScanLine(line string) ([]Token, error) {
-	l.CurrentPosition = 0
+	l.currLine += 1
+	l.curr = 0
+	l.tokens = []Token{}
 	if line == "" {
-		return []Token{}, nil
+		return l.tokens, nil
 	}
 
-	return []Token{}, nil
+	for {
+		l.start = l.curr
+		if l.curr >= int64(len(line)) {
+			break
+		}
+		l.scanToken(line)
+	}
+
+	return l.tokens, nil
+}
+
+func (l *Lexer) scanToken(line string) {
+	ch := line[l.curr]
+	var token Token
+
+	switch ch {
+	case ';':
+		token = l.addToken(line, SEMICOLON)
+	}
+
+	l.tokens = append(l.tokens, token)
+	l.advance()
+}
+
+func (l *Lexer) addToken(line string, tokenType TokenType) Token {
+	ch := line[l.start : l.curr+1]
+	token := Token{
+		Type:    tokenType,
+		Literal: ch,
+		Lexeme:  ch,
+		Line:    l.currLine,
+	}
+	return token
+}
+
+// func (l *Lexer) addToken(line, tokenType TokenType, literal interface{}) {
+// 	token := Token{
+// 		Type:    tokenType,
+// 		Literal: literal,
+// 		Lexeme:  String(ch),
+// 		Line:    l.currLine,
+// 	}
+
+// }
+
+func (l *Lexer) advance() {
+	l.curr += 1
 }
