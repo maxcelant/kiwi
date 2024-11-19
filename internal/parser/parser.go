6db0a229e44e0a620e24cb100febb29edad564eb
Max Celant <massimiliano.celant@aa.com>
2024-11-18 20:24:43 -0600

    First updates to the parser

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,6 +1,10 @@
 package parser
 
-import "github.com/maxcelant/kiwi/internal/lexer"
+import (
+	"fmt"
+
+	"github.com/maxcelant/kiwi/internal/lexer"
+)
 
 type Parser struct {
 	tokens  []lexer.Token
@@ -11,6 +15,18 @@ func New(tokens []lexer.Token) *Parser {
 	return &Parser{tokens: tokens, current: 0}
 }
 
-func (p *Parser) parse() ([]Expr, error) {
-	return nil, nil
+func (p *Parser) parse() (Expr, error) {
+	expr, err := p.expression()
+	if err != nil {
+		return nil, fmt.Errorf("parsing error occurred: %w", err)
+	}
+	return expr, nil
+}
+
+func (p *Parser) expression() (Expr, error) {
+
+}
+
+func (p *Parser) isAtEnd() bool {
+	return p.tokens[p.current].Type == lexer.EOF
 }
--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -3,6 +3,7 @@ package parser
 import (
 	"testing"
 
+	"github.com/maxcelant/kiwi/internal/lexer"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
@@ -13,10 +14,34 @@ func TestParser(t *testing.T) {
 }
 
 var _ = Describe("Parser", func() {
-	Describe("Primary", func() {
-		When("its a list of just one nil token", func() {
-			It("returns a tree with just one primary type node", func() {
 
+	Describe("Empty", func() {
+		When("its an empty list of tokens", func() {
+			It("returns an empty parse tree", func() {
+				tokens := []lexer.Token{}
+				parser := New(tokens)
+				actual, err := parser.parse()
+				Expect(err).To(BeNil())
+				Expect(actual).To(Equal())
+			})
+		})
+	})
+
+	Describe("Expressions", func() {
+		Describe("Primary", func() {
+			When("its a list of just one nil token", func() {
+				It("returns a tree with just one primary type node", func() {
+					tokens := []lexer.Token{{
+						Type:    lexer.NIL,
+						Literal: "nil",
+						Lexeme:  "nil",
+						Line:    1,
+					}}
+					parser := New(tokens)
+					actual, err := parser.parse()
+					Expect(err).To(BeNil())
+					Expect(actual).To(Equal(Primary{value: "nil"}))
+				})
 			})
 		})
 	})
