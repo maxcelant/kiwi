b79ac2111e6d094f5353379773c7696a18d1375a
Max Celant <massimiliano.celant@aa.com>
2024-11-29 19:29:00 -0600

    Fixing parser tests

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -31,12 +31,15 @@ var _ = Describe("Parser", func() {
 		Describe("Primary", func() {
 			When("its a list of just one nil token", func() {
 				It("returns a tree with just one primary type node", func() {
-					tokens := []lexer.Token{{
-						Type:    lexer.NIL,
-						Literal: "nil",
-						Lexeme:  "nil",
-						Line:    1,
-					}}
+					tokens := []lexer.Token{
+						{
+							Type:    lexer.NIL,
+							Literal: "nil",
+							Lexeme:  "nil",
+							Line:    1,
+						},
+						{Type: lexer.EOF, Lexeme: "EOF", Line: 1},
+					}
 					parser := New(tokens)
 					actual, err := parser.Parse()
 					Expect(err).To(BeNil())
@@ -46,12 +49,15 @@ var _ = Describe("Parser", func() {
 
 			When("its a list of just one true token", func() {
 				It("returns a tree with just one primary type node", func() {
-					tokens := []lexer.Token{{
-						Type:    lexer.TRUE,
-						Literal: "true",
-						Lexeme:  "true",
-						Line:    1,
-					}}
+					tokens := []lexer.Token{
+						{
+							Type:    lexer.TRUE,
+							Literal: "true",
+							Lexeme:  "true",
+							Line:    1,
+						},
+						{Type: lexer.EOF, Lexeme: "EOF", Line: 1},
+					}
 					parser := New(tokens)
 					actual, err := parser.Parse()
 					Expect(err).To(BeNil())
@@ -61,12 +67,15 @@ var _ = Describe("Parser", func() {
 
 			When("its a list of just one false token", func() {
 				It("returns a tree with just one primary type node", func() {
-					tokens := []lexer.Token{{
-						Type:    lexer.FALSE,
-						Literal: "false",
-						Lexeme:  "false",
-						Line:    1,
-					}}
+					tokens := []lexer.Token{
+						{
+							Type:    lexer.FALSE,
+							Literal: "false",
+							Lexeme:  "false",
+							Line:    1,
+						},
+						{Type: lexer.EOF, Lexeme: "EOF", Line: 1},
+					}
 					parser := New(tokens)
 					actual, err := parser.Parse()
 					Expect(err).To(BeNil())
@@ -76,12 +85,15 @@ var _ = Describe("Parser", func() {
 
 			When("its a list of just one string token", func() {
 				It("returns a tree with just one primary type node", func() {
-					tokens := []lexer.Token{{
-						Type:    lexer.STRING,
-						Literal: "foo",
-						Lexeme:  "\"foo\"",
-						Line:    1,
-					}}
+					tokens := []lexer.Token{
+						{
+							Type:    lexer.STRING,
+							Literal: "foo",
+							Lexeme:  "\"foo\"",
+							Line:    1,
+						},
+						{Type: lexer.EOF, Lexeme: "EOF", Line: 1},
+					}
 					parser := New(tokens)
 					actual, err := parser.Parse()
 					Expect(err).To(BeNil())
@@ -91,16 +103,95 @@ var _ = Describe("Parser", func() {
 
 			When("its a list of just one number token", func() {
 				It("returns a tree with just one primary type node", func() {
-					tokens := []lexer.Token{{
-						Type:    lexer.NUMBER,
-						Literal: 5,
-						Lexeme:  "5",
-						Line:    1,
-					}}
+					tokens := []lexer.Token{
+						{
+							Type:    lexer.NUMBER,
+							Literal: 5,
+							Lexeme:  "5",
+							Line:    1,
+						},
+						{Type: lexer.EOF, Lexeme: "EOF", Line: 1},
+					}
 					parser := New(tokens)
 					actual, err := parser.Parse()
 					Expect(err).To(BeNil())
 					Expect(actual).To(Equal(&Primary{value: 5}))
+				})
+			})
+		})
+
+		Describe("Unary", func() {
+			When("its a list with a negative and number", func() {
+				It("returns a tree with a unary node", func() {
+					tokens := []lexer.Token{
+						{Type: lexer.MINUS, Lexeme: "-", Line: 1},
+						{Type: lexer.NUMBER, Literal: 5, Lexeme: "5", Line: 1},
+						{Type: lexer.EOF, Lexeme: "EOF", Line: 1},
+					}
+					parser := New(tokens)
+					actual, err := parser.Parse()
+					Expect(err).To(BeNil())
+					Expect(actual).To(Equal(&Unary{
+						operator: tokens[0],
+						right:    &Primary{value: 5},
+					}))
+				})
+			})
+
+			When("its a list with a bang and number", func() {
+				It("returns a tree with a unary node", func() {
+					tokens := []lexer.Token{
+						{Type: lexer.BANG, Lexeme: "!", Line: 1},
+						{Type: lexer.NUMBER, Literal: 5, Lexeme: "5", Line: 1},
+						{Type: lexer.EOF, Lexeme: "EOF", Line: 1},
+					}
+					parser := New(tokens)
+					actual, err := parser.Parse()
+					Expect(err).To(BeNil())
+					Expect(actual).To(Equal(&Unary{
+						operator: tokens[0],
+						right:    &Primary{value: 5},
+					}))
+				})
+			})
+		})
+
+		Describe("Factor", func() {
+			When("its a list with two numbers and an a slash", func() {
+				It("returns a tree with one factor node", func() {
+					tokens := []lexer.Token{
+						{Type: lexer.NUMBER, Literal: 1, Lexeme: "1", Line: 1},
+						{Type: lexer.SLASH, Lexeme: "/", Line: 1},
+						{Type: lexer.NUMBER, Literal: 2, Lexeme: "2", Line: 1},
+						{Type: lexer.EOF, Lexeme: "EOF", Line: 1},
+					}
+					parser := New(tokens)
+					actual, err := parser.Parse()
+					Expect(err).To(BeNil())
+					Expect(actual).To(Equal(&Binary{
+						left:     &Primary{value: 1},
+						operator: tokens[1],
+						right:    &Primary{value: 2},
+					}))
+				})
+			})
+
+			When("its a list with two numbers and an a star", func() {
+				It("returns a tree with one factor node", func() {
+					tokens := []lexer.Token{
+						{Type: lexer.NUMBER, Literal: 1, Lexeme: "1", Line: 1},
+						{Type: lexer.STAR, Lexeme: "*", Line: 1},
+						{Type: lexer.NUMBER, Literal: 2, Lexeme: "2", Line: 1},
+						{Type: lexer.EOF, Lexeme: "EOF", Line: 1},
+					}
+					parser := New(tokens)
+					actual, err := parser.Parse()
+					Expect(err).To(BeNil())
+					Expect(actual).To(Equal(&Binary{
+						left:     &Primary{value: 1},
+						operator: tokens[1],
+						right:    &Primary{value: 2},
+					}))
 				})
 			})
 		})
