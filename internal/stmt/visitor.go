ea349b04e5cef3f0d8a922043aa8ee0654a456b9
Max Celant <massimiliano.celant@aa.com>
2024-12-01 16:28:27 -0600

    return comment

--- internal/stmt/visitor.go
+++ internal/stmt/visitor.go
@@ -1,5 +1,7 @@
 package stmt
 
+// Statements perform "side-effects" and don't actually evaluate
+// to a value, hence why we don't return a value for this interface
 type Visitor interface {
 	VisitPrintStatement(Stmt) error
 	VisitExpressionStatement(Stmt) error
