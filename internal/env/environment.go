8a6b2cdf9b323c7696358d7536924af8c4496735
Max Celant <massimiliano.celant@aa.com>
2024-12-15 13:06:37 -0600

    Update environment

--- internal/env/environment.go
+++ internal/env/environment.go
@@ -1,3 +1,5 @@
+// if yes, then assign the new value to that variable int the environment
+// if yes, then assign the new value to that variable int the environment
 package env
 
 import (
@@ -14,6 +16,14 @@ func New() Environment {
 	return Environment{
 		Values: make(map[string]any),
 	}
+}
+
+func (e *Environment) Assign(name lexer.Token, value any) error {
+	if _, ok := e.Values[name.Lexeme]; !ok {
+		return fmt.Errorf("undefined variable: '%s'", name.Lexeme)
+	}
+	e.Values[name.Lexeme] = value
+	return nil
 }
 
 func (e *Environment) Define(name string, value any) {
